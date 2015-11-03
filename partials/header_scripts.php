fe8fc228ab6d38f6fe58a64b09a02dfe4488b2f6
Justin Dian <justin.dian@cision.com>
2015-11-03 08:51:10 -0500

    add GA scripts

--- partials/header_scripts.php
+++ partials/header_scripts.php
@@ -2,3 +2,15 @@
 <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="scripts/bootstrap-3.3.5-dist/css/bootstrap.min.css" />
 <link rel="stylesheet" href="styles/kiosk.css" />
+
+<!-- Universal Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-583116-33', 'auto');
+  ga('send', 'pageview');
+
+</script>
